c01524c88dd96f2eb1383a7ab4392f8a5ac6fba6
Mylo Cyrus <irahiggins740@gmail.com>
2023-07-09 09:34:13 -0700

    Add files via upload

---
+++ Army_Corp_GR_MX-US.jl
@@ -0,0 +1,184 @@
+Log: Log file open, 06/04/23 10:14:45
+Init: Name subsystem initialized
+Init: Detected: Microsoft Windows NT 6.1 (Build: 7601)
+Init: Unreal Version: 927
+Init: AGP Version: 927.1.5.0
+Init: Compiled: Dec 20 2002 10:29:26
+Init: Command line: 
+Init: Base directory: C:\Program Files (x86)\Army Operations\System\
+Init: Character set: Unicode
+Log: Bound to Window.dll
+Log: Bound to Core.dll
+Log: Bound to Engine.dll
+Init: Object subsystem initialized
+Init: Computer: MYLOCYRUS
+Init: User: mylo
+Init: Memory total: Phys=3004688K Pagef=4194303K Virt=2097024K
+Init: Working set: 32000 / 159000
+Init: CPU Page size=4096, Processors=1
+Init: CPU Detected: PentiumPro-class processor (GenuineIntel)
+Init: CPU Features: CMov FPU RDTSC PAE MMX SSE
+Init: CPU Speed=1861.951735 MHz
+Init: Memory total: Phys=3004688K Pagef=4194303K Virt=2097024K
+Init: Working set: 32000 / 159000
+Localization: No localization: ArmyOps.General.Start (int)
+Init: Unreal engine initialized
+Log: Bound to WinDrv.dll
+Init: Client initialized
+Log: Bound to D3DDrv.dll
+Init: Direct3D adapters detected:
+Log: 	igdumdx32.dll/Mobile Intel(R) 4 Series Express Chipset Family
+Init: D3D Driver: MaxTextureBlendStages=8
+Init: D3D Driver: MaxSimultaneousTextures=8
+Init: D3D Driver: MaxActiveLights=8
+Init: D3D Driver: MaxPrimitiveCount=1048575
+Init: D3D Driver: MaxVertexIndex=16777215
+Init: D3D Driver: MaxStreams=16
+Init: D3D Driver: MaxStreamStride=255
+Init: D3D Driver: MaxVertexShaderConst=256
+Init: D3D Driver: D3DVSDT_UBYTE4 allowed: Yes.
+Init: D3D Driver: VertexShaderVersion=1.1
+Init: D3D Driver: PixelShaderVersion=1.4
+Init: D3D Driver: MaxUserClipPlanes=6
+Init: D3D Driver: WARNING: no AGP support detected
+Init: D3D Driver: Supports trilinear
+Init: D3D Driver: Supports BLENDDIFFUSEALPHA
+Init: D3D Driver: Supports LOD biasing
+Init: D3D Driver: Supports Z biasing
+Init: D3D Driver: Supports D3DTOP_ADDSIGNED2X
+Init: D3D Driver: Supports D3DTOP_BUMPENVMAP
+Init: D3D Driver: Supports D3DTOP_BUMPENVMAPLUMINANCE
+Init: D3D Driver: Supports D3DTOP_DOTPRODUCT3
+Init: D3D Driver: Supports D3DTOP_MODULATEALPHA_ADDCOLOR
+Init: D3D Driver: Supports D3DTOP_MODULATECOLOR_ADDALPHA
+Init: D3D Driver: Textures (4096x4096), Max aspect 2048
+Init: Unreal Engine Direct3D support - internal version: SB2
+Init: D3D Device: szDriver=igdumdx32.dll
+Init: D3D Device: szDescription=Mobile Intel(R) 4 Series Express Chipset Family
+Init: D3D Device: wProduct=8
+Init: D3D Device: wVersion=15
+Init: D3D Device: wSubVersion=10
+Init: D3D Device: wBuild=2413
+Init: D3D Device: dwVendorId=32902
+Init: D3D Device: dwDeviceId=10818
+Init: D3D Device: dwSubSysId=820711484
+Init: D3D Device: dwRevision=7
+Init: D3D Detected: Intel video card
+Init: D3D Device: using cubemaps [with mipmaps]
+Log: LoadMap: Entry
+Log: Bound to AGP.dll
+Log: Bound to IpDrv.dll
+Log: Game class is 'AGP_GameSinglePlayer'
+Log: Initializing PhysicsVolumes list
+Log: Level is Level Entry.MyLevel
+Log: *** WARNING - PATHS ARE VALID ***
+Log: Bringing Level Entry.MyLevel up for play (0)...
+ScriptLog: InitGame: National CD Rateline
+608 Middle Country Road
+Middle Island, NY 11953-2544
+Call (800) 246-0005
+www.cdrateline.com
+National CD Rateline is a
+jumbo CD rate-listing service connecting the investor
+and depository institutions directly. Depositors
+list their rates on our secure web site and investor clients (banks and credit unions) place funds
+directly. Unlimited funding or investments. Low
+annual subscription rate. No transaction fees.
+Non-brokered. Complete accounting package.
+ScriptLog: ### MutatorClass: Engine.Mutator
+ScriptLog: Base Mutator is Entry.Mutator0
+ScriptLog: LevelInfo::BeginPlay() - Entry.LevelInfo0
+ScriptLog: AGP_GameInfo::LoadTourInfo - iNumTours 9
+ScriptLog: *************>>>> LevelName:Entry Tour:-1 Mission:-1 Sequential:False
+Log: Browse: Entry.aao?Name=Recruit?Class=AGP_Characters.AGP_Character?team=255
+Log: LoadMap: Entry.aao?Name=Recruit?Class=AGP_Characters.AGP_Character?team=255
+Log: Possessed PlayerPawn: HumanController Entry.HumanController0
+Log: HumanController0 got player WindowsViewport0
+ScriptLog: **** Screenplay::DumpScreenplay() FINISH ****
+ScriptLog: ------------ERROR------------
+ScriptLog: HumanController::ClientRestart(Mexican GR Benets)	Success
+ScriptLog: ------------ERROR------------
+ScriptLog: HumanController::LoadJacket()
+ScriptLog: Failed opening for read save\UserName.txt
+ScriptLog: HumanController::SaveJacket()
+ScriptLog: MainMenuFrame::Create - creating StaticMainMenu screen
+ScriptLog: MainMenuFrame::Create - creating main screen
+ScriptLog: MainMenuFrame::Create - training screen
+ScriptLog: MainMenuFrame::Create - Tour screen
+ScriptLog: MainMenuFrame::Create - History screen
+ScriptLog: MainMenuFrame::Create - MissionBriefSP screen
+ScriptLog: MainMenuFrame::Create - MissionBriefMP screen
+ScriptLog: MainMenuFrame::Create - mp main screen
+ScriptLog: MainMenuFrame::Create - login screen
+ScriptLog: MainMenuFrame::Create - server browser screen
+ScriptLog: MainMenuFrame::Create - settings screen
+ScriptLog: MainMenuFrame::CreateCreditsScreens(credits.ini)
+ScriptLog: UITextPageDisplay::SetTextFile(C:\Program Files (x86)\Army Operations\System\credits.ini)
+ScriptLog: MainMenuFrame::CreateCreditsScreens(creditsarmy.ini)
+ScriptLog: UITextPageDisplay::SetTextFile(C:\Program Files (x86)\Army Operations\System\creditsarmy.ini)
+ScriptLog: MainMenuFrame::Create - getting started screen
+ScriptLog: MainMenuFrame::Create - settings screen
+ScriptLog: MainMenuFrame - AddMouse
+ScriptLog: TestFrame::Create - creating main screen
+Init: WinSock: version 1.1 (2.2), MaxSocks=32767, MaxUdp=65467
+Init: WinSock: I am mylocyrus (192.168.34.49)
+ScriptLog: ClientBeaconReceiver initialized.
+Log: Bound to ALAudio.dll
+Init: ALAudio: subsystem initialized.
+Log: ALAUDIO level okay, checking 3 actors in Untitled.
+Init: Input system initialized for WindowsViewport0
+Log: Opened viewport
+Log: Enter SetRes: 800x600 Fullscreen 1
+Init: Best-match display mode: 800x600x32@60 (Error=0)
+Log: Using back-buffer format 22(32-bit)
+Log: Using depth-buffer format 75(32-bit)
+Log: Creating device
+Init: D3D Driver: CreateDevice: will use hardware transform and lighting.
+Init: D3D Driver: CreateDevice: will use hardware vertex processing
+Init: Game engine initialized
+Log: Startup time: 38.948593 seconds
+Localization: No localization: ArmyOps.General.Run (int)
+Log: Allocating 16384 byte dynamic index buffer.
+Log: Allocating 65536 byte dynamic vertex buffer.
+ScriptLog: AGP_InventoryModifier::Run() invEmpty running on AGP_Character
+ScriptLog: AGP_InventoryModifier::Run() Emptying Inventory
+ScriptLog: HumanController::AGPMainMenu()
+Log: EndFullscreen
+Log: Enter SetRes: 800x600 Fullscreen 0
+Log: Current display mode is 32 bits (22)
+Log: Using back-buffer format 22(32-bit)
+Log: Using depth-buffer format 75(32-bit)
+Log: Viewport WindowsViewport0: WM_DisplayChange
+Log: Creating device
+Init: D3D Driver: CreateDevice: will use hardware transform and lighting.
+Init: D3D Driver: CreateDevice: will use hardware vertex processing
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Allocating 65536 byte dynamic vertex buffer.
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/soldiers/news/
+Log: Browse: http://www.goarmy.com/
+Log: Browse: http://www.goarmy.com/
+Log: Browse: http://www.americasarmy.com/
+Log: Browse: http://www.americasarmy.com/
+Log: Browse: http://americasarmy.com/soldiers/news/
+Log: Browse: http://www.americasarmy.com/faq.php?section=Parents
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Browse: http://americasarmy.com/ops/agp_signup.php
+Log: Enter SetRes: 800x600 Fullscreen 0
+Log: Current display mode is 32 bits (22)
+Log: Using back-buffer format 22(32-bit)
+Log: Using depth-buffer format 75(32-bit)
+Log: Creating device
+Init: D3D Driver: CreateDevice: will use hardware transform and lighting.
+Init: D3D Driver: CreateDevice: will use hardware ver
